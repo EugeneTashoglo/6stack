fda89595245600f4e082aaaebff449dae12db41d
Евгений Ташогло <tashoglo00@mail.ru>
2022-05-05 09:16:07 +0200

    Добавьте файлы проекта.

---
+++ WpfApp2/Monitor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    public class Monitor
+    {
+        public string Name { get; set; }
+       
+        public decimal Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Название:{Name}, " +
+                
+                $"Цена:{Price}";
+        }
+    }
+}
